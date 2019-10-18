a4914da12591d40792a62d0fac0252efd31ddaa8
JustinRowsell <Rowsell64@gmail.com>
2019-10-17 20:18:45 -0600

    lecture 19

--- MyPortfolio/src/components/MainMenu.js
+++ MyPortfolio/src/components/MainMenu.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { graphql, StaticQuery, Link } from 'gatsby';
 import styled from 'styled-components';
+import SiteInfo from './SiteInfo';
 
 const MainMenuWrapper = styled.div`
   display: flex;
   background-color: rgb(3, 27, 77);
-`
+`;
 const MenuItem = styled(Link)`
-  color: white;
+  color: white !important;
   display: block;
   padding: 8px 16px;
-`
+`;
+
+const MainMenuInner = styled.div`
+  max-width: 960px;
+  margin: 0 auto;
+  display: flex;
+  width: 960px;
+  height: 100%;
+`;
+
 const MainMenu = () => (
     <StaticQuery query={graphql`
     {
@@ -32,11 +42,14 @@ const MainMenu = () => (
       }`
     } render={props => (
         <MainMenuWrapper>
+          <MainMenuInner>
+            <SiteInfo />
             {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item => {
                 return <MenuItem to={item.object_slug} key={item.title}>
                     {item.title}
                 </MenuItem>
             })}
+          </MainMenuInner>
         </MainMenuWrapper>
     )} />
 );
