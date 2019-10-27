56f1ff769b219d9bcab70299a3f81832cb548792
JustinRowsell <Rowsell64@gmail.com>
2019-10-27 15:43:03 -0600

    lecture 27

--- MyPortfolio/src/templates/portfolio.js
+++ MyPortfolio/src/templates/portfolio.js
@@ -5,14 +5,20 @@ import styled from 'styled-components';
 const FeaturedImage = styled.img`
     max-width: 300px;
     margin: 16px 0;
-`
+`;
 
 export default ({pageContext}) => (
     <Layout>
         <h1>
             {pageContext.title}
         </h1>
-        <FeaturedImage src={pageContext.featred_media.source_url} />
+        <strong>
+            Website url:
+        </strong>
+        <a href={pageContext.acf.portfolio_url} target="blank">
+            {pageContext.acf.portfolio_url}
+        </a>
+        <FeaturedImage src={pageContext.featured_media.source_url} />
         <div dangerouslySetInnerHTML={{__html: pageContext.content}} />
     </Layout>
 );
